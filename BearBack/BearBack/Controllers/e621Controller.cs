d13772e502fb31d65ef6a67f99298085b92c1e73
Evan Spiker <umdoobby@gmail.com>
2020-12-11 20:37:49 -0500

    Started making a new dashboard - upgrade to bootstrap 5

--- BearBack/BearBack/Controllers/e621Controller.cs
+++ BearBack/BearBack/Controllers/e621Controller.cs
@@ -1,7 +1,36 @@
-﻿namespace BearBack.Controllers
+﻿using System.Diagnostics;
+using BearBack.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BearBack.Controllers
 {
-    public class e621Controller
+    public class e621Controller : Controller
     {
+        private readonly IWebHostEnvironment _webHost;
+        private readonly ILogger<e621Controller> _logger;
+
+        public e621Controller(ILogger<e621Controller> logger, IWebHostEnvironment webHost)
+        {
+            _logger = logger;
+            _webHost = webHost;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
         
+        
+        /// <summary>
+        /// Default error view
+        /// </summary>
+        /// <returns>Generic error page</returns>
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
     }
 }
---
+++ BearBack/BearBack/Models/e621Models/IndexModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BearBack.Services.e621_e926;
+
+namespace BearBack.Models.e621Models
+{
+    public partial class IndexModel
+    {
+        public List<Post> Posts { get; set; }
+    }
+}
