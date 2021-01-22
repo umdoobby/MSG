f3aa79b1d1fc1ee140a93a1c6839839a43307811
umdoobby <umdoobby@gmail.com>
2021-01-22 17:38:54 -0500

    Created new project

---
+++ Msg/MsgDesktop/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using MsgDesktop.ViewModels;
+using MsgDesktop.Views;
+
+namespace MsgDesktop
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# bears-den
+# MSG Image Board Client
 Desktop browser for e621.
 Well thats the plan anyway... See what actually happens as I work on this.
